b897ad8b6812384e1ff1dfc20521e5cb60f4f5d3
FFFace <FFFace@users.noreply.github.com>
2021-02-20 12:31:49 +0900

    2021.02.20
    미니맵 추가
    
    폴더 정리

---
+++ Assets/Scripts/Other/Minimap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private void Update()
+    {
+        Vector3 pos = target.position;
+        pos.y = 20;
+        transform.position = pos;
+    }
+}
